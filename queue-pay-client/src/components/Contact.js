b4e29c9da65168d817c6f6c8d83a089d2df7db15
Jideofor Nwoke <njusticej@gmail.com>
2020-03-03 17:16:22 +0100

    add contact component

---
+++ queue-pay-client/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const Contact = () => {
+    return (
+        <div>
+        <NavBar/>
+            <h1>Services Page</h1>
+        </div>
+    );
+};
+
+export default Contact;
