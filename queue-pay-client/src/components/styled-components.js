7432216fbbe7cccb37e77a6d5c60005973fd9ac0
Jideofor Nwoke <njusticej@gmail.com>
2020-03-03 20:00:56 +0100

    add styles for login and signup components

--- queue-pay-client/src/components/styled-components.js
+++ queue-pay-client/src/components/styled-components.js
@@ -55,49 +55,167 @@ a{
 `;
 
 export const LoginDiv = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-content: center;
+align-items: center;
+width: 100%;
+height: 83vh;
+
+.username{
     display: flex;
-    flex-direction: column;
-    
-    // justify-content: space-around;
-    background-color: #fffff;
-    height: 100vh;
-    .signin{
-        padding-top:3%;
-        display: flex;
-        justify-content: space-around;
-        background-color: #e6e6e6;
-        width: 30%;
-        height: 30%;
-        text-align: center;
-        margin: 0 auto;
-        flex-direction: column;
-        border-radius: 5px;
-    }
+    justify-items: left;
+}
 
-    .login100-form-title {
-        display: block;
-        font-family: Poppins-Bold;
-        font-size: 39px;
-        color: #333333;
-        line-height: 1.2;
-        text-align: center;
-      }
-      
-    .input-field{
-        width: 80%;
-        margin: 0 auto;
-        
-    }
-    .input{
-        margin: 0 auto;
-        padding: 0;
-        width: 100%;
-        height: 50%;
-    }
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
 
-    .submit-form{
-        margin: 0 auto;
-        width: 30%;
-        height: 100%;
-    }
+  input[type=password], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+  
+  input[type=submit] {
+    width: 100%;
+    background-color: tomato;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  input[type=submit]:hover {
+    background-color: tomato;
+  }
+  
+  div {
+    border-radius: 5px;
+   
+    padding: 20px;
+  }
+
+  a{
+    color: tomato;
+    font-size: .8rem;
+  }
+
+  .util{
+      text-decoration: none;
+  }
+`;
+
+export const RegisterDiv = styled.div`
+display: flex;
+height: 100vh;
+min-width: 80px ;
+width: 60%;
+margin: 0 auto;
+justify-content: space-between;
+
+
+.introduction{
+  width: 45%;
+  padding: 5%;
+  
+}
+.create{
+  width: auto;
+  text-align: left;
+}
+
+.business-summary{
+  width: auto;
+  text-align: justify;
+}
+
+.form-container{
+  display:flex;
+  height: 50%;
+  justify-content: space-between;
+  flex-direction: column;
+  text-align: left;
+  align-items: center;
+  width: 45%;
+  padding: 5%;
+
+}
+
+.signup-form{
+  width: 70%;
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  flex-direction: column;
+}
+
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=password], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=email], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: tomato;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.registered{
+  text-align: center;
+  width: auto;
+}
+
+.util{
+  text-decoration: none;
+}
+
+a{
+  color: tomato;
+  font-size: .9rem;
+}
+
 `;
