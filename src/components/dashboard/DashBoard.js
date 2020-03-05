fedcf6e12ab67a7476beb0f7740c622ffbe378ad
Dolapo47 <adeleyeadedolapomayowa@gmail.com>
2020-03-05 15:12:36 +0100

    create merchant dashboard link layout

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import DashBoardLink from "../dashboard/DashboardLink";
 import {DashboardDiv} from '../styled-components'
 
 const DashBoard = () => {
     return (
         <DashboardDiv>
             <div className ="profile">
-            links
+            <DashBoardLink />
             </div>
             <div className = "pages">
             page
