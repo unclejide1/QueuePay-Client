b7320258d12b86a5d46054daa5b1df601ea3d0e0
Jideofor Nwoke <njusticej@gmail.com>
2020-03-05 12:53:26 +0100

    Add dashboard component

---
+++ src/components/dashboard/DashBoard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {DashboardDiv} from '../styled-components'
+
+const DashBoard = () => {
+    return (
+        <DashboardDiv>
+            <div className ="profile">
+            links
+            </div>
+            <div className = "pages">
+            page
+            </div>
+        </DashboardDiv>
+    );
+};
+
+export default DashBoard;
