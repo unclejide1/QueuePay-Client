7e65126902293518774feeaf8e1bf6bbab41496d
Dolapo47 <adeleyeadedolapomayowa@gmail.com>
2020-03-08 13:55:03 +0100

    create checkout page

---
+++ src/components/Checkout.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import CheckoutForm from "./CheckoutForm";
+import {LuhnCheckCard} from "../Util/util";
+
+
+const Checkout = () => {
+const [inputs, setInputs] = useState({
+    "isLuhn": false,
+    "cardNo": "",
+    "cardHolderName": "",
+    "expiryMonth": "",
+    "expiryYear": "",
+    "cvc": "",
+    "notValid": false
+})
+
+const handleInput = (event) => {
+    if(event){
+        event.persist();
+        setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+    }
+}
+
+const handleSubmit = (event) => {
+    if(event){
+        event.preventDefault();
+        if(LuhnCheckCard((inputs.cardNo))){
+            console.log({"isLuhn":true})
+            setInputs({...inputs,   "isLuhn": true})
+        }else{
+            console.log({"isLuhn":false})
+            setInputs({...inputs,   "isLuhn": false})
+        }
+    }
+}
+
+    return (
+        <div>
+            <CheckoutForm handleInput = {handleInput} inputs={inputs} handleSubmit={handleSubmit}/>
+        </div>
+    )
+    }
+
+
+export default Checkout;
