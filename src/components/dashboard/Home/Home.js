b6f5c439e67f1df06c05f81812e0fdf27ea25dfd
Jideofor Nwoke <njusticej@gmail.com>
2020-03-12 15:25:22 +0100

    Add widgets

--- src/components/dashboard/Home/Home.js
+++ src/components/dashboard/Home/Home.js
@@ -6,18 +6,18 @@ const Home = () => {
     return (
         <HomeDiv>
         <div className ="details">
-        <div className = "value transact">
-        <h3>Total Transactions</h3>
-        <h4>10,000</h4>
-        </div>
-        <div className ="value val">
-        <h3>Total Value</h3>
-        <h4>15,000</h4>
-        </div>
-        <div className ="value bal">
-        <h3>Balance</h3>
-        <h4>25,000</h4>
-        </div>   
+            <div className = "value transact">
+            <h3>Total Transactions</h3>
+            <h4>10,000</h4>
+            </div>
+            <div className ="value val">
+            <h3>Total Value</h3>
+            <h4>15,000</h4>
+            </div>
+            <div className ="value bal">
+            <h3>Balance</h3>
+            <h4>25,000</h4>
+            </div>   
         </div>
         <div className = "piechart">
         <h1>Transactions Details</h1>
--- src/components/dashboard/ProcessPayment/ProcessPayment.js
+++ src/components/dashboard/ProcessPayment/ProcessPayment.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PaymentWidget from '../../PaymentWidget';
+import PaymentWidget from '../../../PaymentWidget';
 import OTP from '../../../components/OTP';
 
 export default function ProcessPayment() {
