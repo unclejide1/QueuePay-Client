ba9d118dd556a4835c39242e3738b651b98b42f9
Jideofor Nwoke <njusticej@gmail.com>
2020-03-12 15:24:23 +0100

    modified transaction component to be dynamic

--- src/components/dashboard/Transaction.js
+++ src/components/dashboard/Transaction.js
@@ -1,39 +1,59 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+import {Link } from 'react-router-dom';
 import {TransactionDiv} from "../styled-components";
 
 const Transaction = () => {
+    // const history = useHistory();
+    const [transArray, setTransArray] = useState([])
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            const transactions = await fetch('/transactions/' + localStorage.getItem("businessID"), {
+                method: "GET",
+                headers:{
+                  "content-type": 'application/json',
+                  "Authorization": "Bearer " + localStorage.getItem("token")
+                },
+              })
+              const json = await transactions.json()
+              setTransArray(json)
+        }
+       fetchData()
+    }, [])
+
+    
     return (
         <TransactionDiv>
             <div className="transactions">
             <h3>View all transactions</h3>
-            <table class="table table-striped">
+            <table className="table table-striped">
             <thead>
                 <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Customer</th>
+                <th scope="col">Card Type</th>
+                <th scope="col">Status</th>
+                <th scope="col">View</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-                </tr>
+    
+            {transArray.length?transArray.map((res, index)=>
+                (<tr key = {res.id}>
+                <th scope="row">{index + 1}</th>
+                <td>{res.amount}</td>
+                <td>{res.customerName}</td>
+                <td>{res.cardType}</td>
+                <td>{res.status}</td>
+                <Link to={`/dashboard/singletransaction/${res.id}`}><td ><button id={res.id}>View</button></td></Link>
+                </tr>)
+            ):(<tr>
+                <th scope="row">N/A</th>
+                <td>N/A</td>
+                <td>N/A</td>
+                <td>N/A</td>
+                <td>N/A</td>
+                </tr>)}    
             </tbody>
             </table>
             </div>
